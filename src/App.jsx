c059393ba8590f96f591697893674cbdbf7f8568
Jmsteele01 <Jmsteele01@hotmail.com>
2025-05-21 22:50:16 -0600

    removed old pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 // Pages
 import Homepage from "./pages/Homepage";
-import AuthPage from "./pages/AuthPage";
-import CreateForm from "./pages/CreateForm";
 import Dashboard from "./pages/DexSearch";
-import ShowOnePage from "./pages/ShowOnePage";
 import NotFound from "./pages/NotFound";
 
 // Components
@@ -19,10 +16,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/auth" element={<AuthPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create" element={<CreateForm />} />
-        <Route path="/product/:id" element={<ShowOnePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
